60ab77afd023b094593e188b2879cdbe0c9408bb
Rishabhh3 <rishabhchamoli0120@gmail.com>
2026-01-18 23:02:54 +0530

    Fixed all errors

--- src/pipleline/predict_pipeline.py
+++ src/pipleline/predict_pipeline.py
@@ -24,7 +24,7 @@ class PredictPipeline:
 class CustomData:
     def __init__(self,
                  gender:str,
-                 race_ethinicty: int,
+                 race_ethnicity: int,
                  parental_level_of_education,
                  lunch:str,
                  test_preparation_course:str,
@@ -33,7 +33,7 @@ class CustomData:
         
         self.gender = gender
 
-        self.race_ethinicity = race_ethinicty
+        self.race_ethnicity = race_ethnicity
         self.parental_level_of_education = parental_level_of_education
         self.lunch = lunch
         self.test_preparation_course = test_preparation_course
@@ -44,7 +44,7 @@ class CustomData:
         try:
             custom_data_input_dict={
             "gender" : [self.gender],
-            "race_ethinicity":[self.race_ethinicity],
+            "race_ethnicity":[self.race_ethnicity],
             "parental_level_of_education":[self.parental_level_of_education],
             "lunch":[self.lunch],
             "test_preparation_course":[self.test_preparation_course],
